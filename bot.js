d5e991ba5eaa7ce2f549464cd2e28200e859f72f
PraetorDragoon <Praetor.Dragoon@gmail.com>
2018-05-16 11:58:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
     	message.reply('pong');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '!nurgling') {
+    	':BestHeart:'
   	}
 });
 
