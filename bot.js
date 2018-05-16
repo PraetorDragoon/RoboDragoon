7dbd1f3369d4ea1a5dbe917ca5b34addc95126b1
PraetorDragoon <Praetor.Dragoon@gmail.com>
2018-05-16 12:55:20 +0200

    Update bot.js
    changed If/Then to Switch/Case

--- bot.js
+++ bot.js
@@ -4,16 +4,31 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === '!ping') {
-    	message.reply('pong');
-  	}
-});
-client.on('message', message => {
-    if (message.content === '!nurgling') {
-    	message.reply(':BestHeart:')
-  	}
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            case 'nurgling':
+                bot.sendMessage({
+                    to: channelID,
+                    message: ' :BestHeart:'
+                });
+            break;                
+            // Just add any case commands if you want to..
+         }
+     }
 });
 
 // THIS  MUST  BE  THIS  WAY
